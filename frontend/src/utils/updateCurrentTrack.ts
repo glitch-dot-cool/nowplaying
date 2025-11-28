8ef6ad9f4c0e5ebe26befb66cfbccf33a8edd49b
alex gohorel <gohorelalex@gmail.com>
2025-11-28 18:14:40 -0500

    fix: hardcoded base URL in updateTrack

--- frontend/src/utils/updateCurrentTrack.ts
+++ frontend/src/utils/updateCurrentTrack.ts
@@ -1,13 +1,16 @@
 import type { Track } from "../types";
 
 export const updateCurrentTrack = async (newTrack: Track) => {
-  const res = await fetch("http://localhost:3000/update-track", {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(newTrack),
-  });
+  const res = await fetch(
+    `${import.meta.env.VITE_SERVER_BASE_URL}/update-track`,
+    {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newTrack),
+    }
+  );
 
   const data = await res.json();
   return data;
