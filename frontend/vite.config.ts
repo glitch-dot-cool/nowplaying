5b0b2b1d3b135edf2ceb5e8add3bd52e4d97cb74
alex gohorel <gohorelalex@gmail.com>
2025-12-08 17:35:50 -0500

    configurable port for frontend app

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+
+const DEFAULT_PORT = "3001";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react()],
+    server: {
+      port: parseInt(env.VITE_PORT || DEFAULT_PORT),
+    },
+    preview: {
+      port: parseInt(env.VITE_PORT || DEFAULT_PORT),
+    },
+  };
+});
